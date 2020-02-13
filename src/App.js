37bcc48fa644b88f8649937c81cba6e21b094a2f
M.nasiri <nasiri.dev84@gmail.com>
2020-02-13 16:57:11 +0330

    repaire files for chenge component folder

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import Users from '../components/Users';
+import Users from './components/Users';
+
 
 class App extends Component {
  
   render() {
     return (
-      <div>
-       
-        
-            <thead>
-                <tr>        
-                    <th>آدرس گیت هاب</th>
-                    <th>نام کاربری</th>                    
-                    <th>آیدی</th> 
-                    <th>تصویر</th>        
-                </tr>
-            </thead>          
-        
+      <div>   
+               
+        <thead>
+            <tr>        
+                <th>آدرس گیت هاب</th>
+                <th>نام کاربری</th>                    
+                <th>آیدی</th> 
+                <th>تصویر</th>        
+            </tr>
+        </thead>          
         <Users />
+        
       </div>
       
     );
