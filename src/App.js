f99507faff3facf7e5ed14b6b7e138e9e4718090
محمد نصیری <nasiri.dev84@gmail.com>
2020-03-19 19:54:27 +0330

    rewrite functional component

--- src/App.js
+++ src/App.js
@@ -3,26 +3,45 @@ import './App.css';
 import Users from './components/Users';
 
 
-class App extends Component {
+// class App extends Component {
  
-  render() {
-    return (
-      <div>   
+//   render() {
+//     return (
+//       <div>   
                
-        <thead>
-            <tr>        
-                <th>آدرس گیت هاب</th>
-                <th>نام کاربری</th>                    
-                <th>آیدی</th> 
-                <th>تصویر</th>        
-            </tr>
-        </thead>          
-        <Users />
+//         <thead>
+//             <tr>        
+//                 <th>آدرس گیت هاب</th>
+//                 <th>نام کاربری</th>                    
+//                 <th>آیدی</th> 
+//                 <th>تصویر</th>        
+//             </tr>
+//         </thead>          
+//         <Users />
         
-      </div>
+//       </div>
       
-    );
-  }
+//     );
+//   }
+// }
+
+function App(props){
+  return (
+          <div>   
+                   
+            <thead>
+                <tr>        
+                    <th>آدرس گیت هاب</th>
+                    <th>نام کاربری</th>                    
+                    <th>آیدی</th> 
+                    <th>تصویر</th>        
+                </tr>
+            </thead>          
+            <Users />
+            
+          </div>
+          
+        );
 }
 
 export default App;
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,46 +1,75 @@
-import React, { Component } from 'react';
+import React, {useState,useEffect } from 'react';
 import './Users.css';
-class Users extends Component {
-
-    constructor(props) {
-        super(props);
-        this.componentDidMount = this.componentDidMount.bind(this);
-        this.state = {
-          users:[]
-        }
+// class Users extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.componentDidMount = this.componentDidMount.bind(this);
+//         this.state = {
+//           users:[]
+//         }
         
-      }
+//       }
       
-      componentDidMount() {
-        fetch('https://api.github.com/search/users?q=language:javascript')
+//       componentDidMount() {
+//         fetch('https://api.github.com/search/users?q=language:javascript')
+//         .then(res => res.json())
+//         .then(
+//           json =>{
+//             this.setState({
+//               users:json.items
+//             })
+//           }
+//         )       
+        
+//       }
+
+//     render() {
+//         return (
+//             <div>
+//                 {this.state.users.map(items=>(                    
+//                     <tbody>
+//                         <tr>                                    
+//                             <td><a href={items.html_url} target="_blank">{items.html_url}</a></td>
+//                             <td>{items.login}</td>                            
+//                             <td>{items.id}</td>
+//                             <td><img src={items.avatar_url}></img></td>
+//                         </tr>
+//                     </tbody>          
+                
+//                 ))}
+//             </div>
+//         );
+//     }
+// }
+function Users (props){
+  const [state,setState] = useState({users:[]})
+
+  useEffect(()=>{
+    fetch('https://api.github.com/search/users?q=language:javascript')
         .then(res => res.json())
         .then(
           json =>{
-            this.setState({
+            setState({
               users:json.items
             })
           }
-        )       
-        
-      }
+        )        
+  },[])
 
-    render() {
-        return (
-            <div>
-                {this.state.users.map(items=>(                    
-                    <tbody>
-                        <tr>                                    
-                            <td><a href={items.html_url} target="_blank">{items.html_url}</a></td>
-                            <td>{items.login}</td>                            
-                            <td>{items.id}</td>
-                            <td><img src={items.avatar_url}></img></td>
-                        </tr>
-                    </tbody>          
+  return (                
+   state.users.map(items=>(                    
+    <tbody>
+      <tr>                                    
+          <td><a href={items.html_url} target="_blank">{items.html_url}</a></td>
+          <td>{items.login}</td>                            
+          <td>{items.id}</td>
+          <td><img src={items.avatar_url}></img></td>
+      </tr>
+    </tbody>          
+                    
+      ))
                 
-                ))}
-            </div>
-        );
-    }
+            )
 }
 
 export default Users;
